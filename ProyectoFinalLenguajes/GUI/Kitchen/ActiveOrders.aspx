45d4b6b24e5f890bcfed3de2707e6e249aaa60d9
yeanj <yeanj@DESKTOP-D252BMI>
2018-07-11 13:19:42 -0600

    Add Color State table

--- ProyectoFinalLenguajes/GUI/Kitchen/ActiveOrders.aspx
+++ ProyectoFinalLenguajes/GUI/Kitchen/ActiveOrders.aspx
@@ -1,5 +1,4 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Kitchen/KitchenSiteMasterPage.Master" AutoEventWireup="true" CodeBehind="ActiveOrders.aspx.cs" Inherits="GUI.Kitchen.ActiveOrders" %>
-
 <asp:Content ID="Content1" ContentPlaceHolderID="KitchenPanelHead" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="KitchenPanelContent" runat="server">
@@ -7,20 +6,17 @@
     <script src="Content/js/JSKitchenModule.js"></script>
     <div ">
         <br />
-        <br />
-        <h5>List Of Pending Orders</h5>
-        <br />
-        <br />
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Code Order</th>
-                    <th>Name Client</th>
-                    <th>List Dishes</th>
-                </tr>
-            </thead>
-            <tbody id="tbSystemUser">
-            </tbody>
-        </table>
+        <h3>List Of Orders</h3>
+         <table class="table">
+         <thead>
+<tr>
+ <th>#Order</th>
+ <th>Name Client</th>
+ <th>Dishes</th>
+ </tr>
+</thead>
+         <tbody id="user">
+         </tbody>
+         </table>
     </div>
 </asp:Content>
--- ProyectoFinalLenguajes/GUI/Kitchen/Content/js/JSKitchenModule.js
+++ ProyectoFinalLenguajes/GUI/Kitchen/Content/js/JSKitchenModule.js
@@ -20,31 +20,76 @@ function getAllOrder() {
 }
 
 //function processAllOrder(data) {
+//    $('#user').empty();
+
+//    var newTitu = document.createElement("h3");
+//    newTitu.innerHTML = "List Of Orders";
+//    $('#user').append(newTitu);
 //    $.each(data, function () {
-//        var newTR = document.createElement("tr");
+//        var newLi = document.createElement("li");
+//        var newInfo = document.createElement("h5");
+//        newInfo.innerHTML = "Order Code: " + this.OrderCode + ". Name Client: " + this.ClientName + ". Dishes: " + this.dishOrder;
+//        newLi.appendChild(newInfo);
+//        var boton = document.createElement('input');
+//        boton.type = 'button';
+//        boton.id = 'B';
+//        boton.value = 'Entregar';
+//        boton.onclick = "";
 
-//        var newTD1 = document.createElement("td");
-//        newTD1.innerHTML = this.OrderCode;
-//        var newTD2 = document.createElement("td");
-//        newTD2.innerHTML = this.ClientName;
-//        var newTD3 = document.createElement("td");
-//        newTD3.innerHTML = this.dishOrder;
+//        newLi.appendChild(boton);
+//        newLi.appendChild(document.createElement("hr"));
 
-
-//        //var boton = document.createElement("button");
-//        //boton.type = "button";
-//        //newTD4.appendChild(boton);
-
-//        newTR.appendChild(newTD1);
-//        newTR.appendChild(newTD2);
-//        newTR.appendChild(newTD3);
-
-//        $('#tbSystemUser').append(newTR);
-//    })
+//        $('#user').append(newLi);
+//    });
 //};
 
 function processAllOrder(data) {
-    for (var i = 0; i < 5; i++) {
-        console.log('intento ' + i);
-    }
-};
+    //$('#user').empty();
+
+    //var newTitu = document.createElement("h3");
+    //newTitu.innerHTML = "List Of Orders";
+    //$('#user').append(newTitu);
+    $.each(data, function () {
+        var newTR = document.createElement("tr");
+
+        switch(this.OrderState) {
+            case "A tiempo":
+                newTR.style.background = "#01DF01";
+                break;
+            case "Sobre Tiempo":
+                newTR.style.background = "#FFFF00";
+                break;
+            default:
+                newTR.style.background = "#FF0000";
+                break;
+                
+        }
+        var newTD1 = document.createElement("td");
+        newTD1.innerHTML = this.OrderCode;
+        var newTD2 = document.createElement("td");
+        newTD2.innerHTML = this.ClientName
+        var newTD3 = document.createElement("td");
+        newTD3.innerHTML = this.dishOrder;
+
+        newTR.appendChild(newTD1);
+        newTR.appendChild(newTD2);
+        newTR.appendChild(newTD3);
+
+        var newTD4 = document.createElement("td");
+        var boton = document.createElement('input');
+        boton.type = 'button';
+        boton.id = 'B';
+        boton.value = 'Deliver';
+        boton.onclick = "";
+
+        newTD4.appendChild(boton);
+        newTR.appendChild(newTD4);
+        $('#user').append(newTR);
+    });
+}
+
+function reloadSystemUsersTable() {
+    setInterval(getAllSystemUsers, 1000);
+}
+
+
